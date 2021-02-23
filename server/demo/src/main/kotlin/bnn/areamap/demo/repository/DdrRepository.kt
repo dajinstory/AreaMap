2e3806ed39f8f459e73b58012ab7595928057373
Dajin Han <dajinstory@naver.com>
2021-02-23 20:55:49 +0900

    kotlin-spring inmemory DB

--- server/demo/src/main/kotlin/bnn/areamap/demo/repository/DdrRepository.kt
+++
@@ -1,7 +0,0 @@
-package bnn.areamap.demo.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface DdrRepository : JpaRepository<Ddr?, Long?> {
-    override fun findAll(): List<Ddr?>
-}
